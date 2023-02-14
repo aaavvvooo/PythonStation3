6505017897e86ed20d0e529cac486594df8a7933
Avo Midoyan <amidoyan@c2r7s5.42yerevan.am>
2023-02-14 21:10:12 +0400

    fixed when minute or second is >= 60

--- countdown.py
+++ countdown.py
@@ -17,6 +17,9 @@ def get_input()->list[int]:
 
 def main():
     args = get_input()
+    if args[2] < 0 or args[2] >= 60 or args[1] < 0 or args[1] >= 60:
+        print("Wrong input")
+        exit(1)
     sum = args[0] * 3600 + args[1] * 60 + args[2]
     while sum >= 0:
         print(f"{sum // 3600}:{(sum // 60 % 60)}:{sum % 60}")
